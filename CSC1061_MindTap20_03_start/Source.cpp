37985771f55da0bc6743bbc71e33437f11d38e47
kmgei <kmgei@DORI>
2024-03-31 08:51:54 -0600

    Add project files.

---
+++ CSC1061_MindTap20_03_start/Source.cpp
@@ -0,0 +1,23 @@
+// CSC1061_MindTap20_02
+// Depth First Traversal and Breadth First Traversal
+#include <iostream>
+#include <fstream>
+#include "graphType.h" 
+#include "weightedGraph.h"
+
+using namespace std;
+
+int main()
+{
+	// To Do
+	// 1) Create an instance of a weighted graph
+	// 2) Call the appropriate function to create
+	//      the graph and its weight matrix
+	// 3) Find the shortest path
+	// 4) Print the shorest distance
+	// Note: the functions are complete and 
+	//       are ready to use.
+
+	return 0;
+
+}
---
+++ CSC1061_MindTap20_03_start/graphType.h
@@ -0,0 +1,270 @@
+#ifndef H_graph
+#define H_graph
+
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include "linkedList.h"
+#include "unorderedLinkedList.h"
+#include "linkedQueue.h"
+
+using namespace std;
+
+// Because there are n nodes, we use an array, A, of size n, 
+// such that A[i] is a reference variable pointing to the first node 
+// of the linked list containing the vertices to which  is adjacent. 
+// Each node has two components, vertex and link. 
+// The component vertex contains the index of the vertex adjacent to vertex i.
+
+class graphType
+{
+public:
+    bool isEmpty() const;
+    //Function to determine whether the graph is empty.
+    //Postcondition: Returns true if the graph is empty;
+    //               otherwise, returns false.
+
+    void createGraph();
+    //Function to create a graph.
+    //Postcondition: The graph is created using the  
+    //               adjacency list representation.
+
+    void clearGraph();
+    //Function to clear graph.
+    //Postcondition: The memory occupied by each vertex 
+    //               is deallocated.
+
+    void printGraph() const;
+    //Function to print graph.
+    //Postcondition: The graph is printed.
+
+    void depthFirstTraversal();
+    //Function to perform the depth first traversal of
+    //the entire graph.
+    //Postcondition: The vertices of the graph are printed 
+    //               using depth first traversal algorithm.
+
+    void dftAtVertex(int vertex);
+    //Function to perform the depth first traversal of 
+    //the graph at a node specified by the parameter vertex.
+    //Postcondition: Starting at vertex, the vertices are 
+    //               printed using depth first traversal 
+    //               algorithm.
+
+    void breadthFirstTraversal();
+    //Function to perform the breadth first traversal of
+    //the entire graph.
+    //Postcondition: The vertices of the graph are printed 
+    //               using breadth first traversal algorithm.
+
+    graphType(int size = 0);
+    //Constructor
+    //Postcondition: gSize = 0; maxSize = size;
+    //               graph is an array of pointers to linked
+    //               lists.
+
+    ~graphType();
+    //Destructor
+    //The storage occupied by the vertices is deallocated.
+
+protected:
+    int maxSize;    //maximum number of vertices
+    int gSize;      //current number of vertices
+    unorderedLinkedList<int>* graph; // adjacency linked list 
+
+private:
+    void dft(int v, bool visited[]);
+    //Function to perform the depth first traversal of 
+    //the graph at a node specified by the parameter vertex.
+    //This function is used by the public member functions
+    //depthFirstTraversal and dftAtVertex.
+    //Postcondition: Starting at vertex, the vertices are 
+    //               printed using depth first traversal 
+    //               algorithm.
+};
+
+bool graphType::isEmpty() const
+{
+    return (gSize == 0);
+}
+
+void graphType::createGraph()
+{
+    ifstream infile;
+    char fileName[50];
+
+    int index;
+    int vertex;
+    int adjacentVertex;
+
+    if (gSize != 0)	//if the graph is not empty, make it empty
+        clearGraph();
+
+    cout << "Enter input file name (Ch20_Ex2Data.txt or Ch20_Ex14Data.txt): ";
+    cin >> fileName;
+    cout << endl;
+
+    infile.open(fileName);
+
+    if (!infile)
+    {
+        cout << "Cannot open input file." << endl;
+        return;
+    }
+
+    infile >> gSize;  //get the number of vertices
+
+    // For each vertex, read data from file and update an
+    // unordered linked list with adjacent vertices
+
+    for (index = 0; index < gSize; index++)
+    {
+        infile >> vertex;
+        infile >> adjacentVertex;
+
+        while (adjacentVertex != -999)
+        {
+            // For each vertex, build an adjacency linked list
+            graph[vertex].insertLast(adjacentVertex);
+
+            // read the next vertex's data
+            infile >> adjacentVertex;
+        } //end while
+    } // end for
+
+    infile.close();
+} //end createGraph
+
+void graphType::clearGraph()
+{
+    int index;
+
+    for (index = 0; index < gSize; index++)
+        graph[index].destroyList();
+
+    gSize = 0;
+} //end clearGraph
+
+
+void graphType::printGraph() const
+{
+    int index;
+
+    for (index = 0; index < gSize; index++)
+    {
+        cout << index << " ";
+        graph[index].print();
+        cout << endl;
+    }
+
+    cout << endl;
+} //end printGraph
+
+void graphType::depthFirstTraversal()
+{
+    bool* visited; //pointer to create the array to keep 
+    //track of the visited vertices
+    visited = new bool[gSize];
+
+    int index;
+
+    for (index = 0; index < gSize; index++)
+        visited[index] = false;
+
+    //For each vertex that is not visited, do a depth 
+    //first traverssal
+    for (index = 0; index < gSize; index++)
+        if (!visited[index])
+            dft(index, visited);
+    delete[] visited;
+} //end depthFirstTraversal
+
+void graphType::dft(int v, bool visited[])
+{
+    visited[v] = true;
+    cout << " " << v << " ";  //visit the vertex
+
+    linkedListIterator<int> graphIt;
+
+    //for each vertex adjacent to v
+    for (graphIt = graph[v].begin(); graphIt != graph[v].end();
+        ++graphIt)
+    {
+        int w = *graphIt;
+        if (!visited[w])
+            dft(w, visited);
+    } //end while
+} //end dft
+
+void graphType::dftAtVertex(int vertex)
+{
+    bool* visited;
+
+    visited = new bool[gSize];
+
+    for (int index = 0; index < gSize; index++)
+        visited[index] = false;
+
+    dft(vertex, visited);
+
+    delete[] visited;
+} // end dftAtVertex
+
+
+void graphType::breadthFirstTraversal()
+{
+    linkedQueueType<int> queue;
+
+    bool* visited;
+    visited = new bool[gSize];
+
+    for (int ind = 0; ind < gSize; ind++)
+        visited[ind] = false;	//initialize the array 
+    //visited to false
+
+    linkedListIterator<int> graphIt;
+
+    for (int index = 0; index < gSize; index++)
+        if (!visited[index])
+        {
+            queue.addQueue(index);
+            visited[index] = true;
+            cout << " " << index << " ";
+
+            while (!queue.isEmptyQueue())
+            {
+                int u = queue.front();
+                queue.deleteQueue();
+
+                for (graphIt = graph[u].begin();
+                    graphIt != graph[u].end(); ++graphIt)
+                {
+                    int w = *graphIt;
+                    if (!visited[w])
+                    {
+                        queue.addQueue(w);
+                        visited[w] = true;
+                        cout << " " << w << " ";
+                    }
+                }
+            } //end while
+        }
+
+    delete[] visited;
+} //end breadthFirstTraversal
+
+    //Constructor
+graphType::graphType(int size)
+{
+    maxSize = size;
+    gSize = 0;
+    graph = new unorderedLinkedList<int>[size];
+}
+
+//Destructor
+graphType::~graphType()
+{
+    clearGraph();
+}
+
+#endif
